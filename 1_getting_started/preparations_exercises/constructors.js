ba25ccaa2560a4386c68bcd137dbf92a62e573ca
EC2 Default User <ec2-user@ip-172-31-40-28.us-east-2.compute.internal>
2020-06-08 16:41:25 +0000

    finished the getting started section, moving on

---
+++ 1_getting_started/preparations_exercises/constructors.js
@@ -0,0 +1,10 @@
+/* 
+
+Identify the Constructors for each of the following methods and classify each method as either a "Static" or a "Prototype" method:
+
+substring = prototype
+create = static
+fromCharCode = static
+slice = prototype
+
+*/
